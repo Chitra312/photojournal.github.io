3a9afcc314933ebc204a9c824701a128429bec9c
Chitra312 <chitrasingh312@gmail.com>
2023-09-15 19:00:59 +0530

    commit

---
+++ src/components/BannerImage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './master.css';
+
+function BannerImage(){
+    return(
+        <div>
+            <img className='banner' src="banner.jpg" alt="Banner Image"/>
+        </div>
+    )
+}
+
+export default BannerImage;
